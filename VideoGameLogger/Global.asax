19b3c1dc04172f424a83184b549352f3fe419476
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-04-10 14:55:46 -0500

    Add project files.

---
+++ VideoGameLogger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoGameLogger.MvcApplication" Language="C#" %>
---
+++ VideoGameLogger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoGameLogger.Startup))]
+namespace VideoGameLogger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

d42965aada504a3901f2df51631ea583be76a8f5
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-04-10 16:38:04 -0500

    A Fighting Video Game Logger
    Log your wins, losses, best time, character names and more with this
    Application!

---
+++ VideoGameLogger/Models/Character.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoGameLogger.Models
+{
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public string CharacterName { get; set; }
+    }
+}
---
+++ VideoGameLogger/Models/VideoGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoGameLogger.Models
+{
+    public class VideoGame
+    {
+        public int VideoGameID { get; set; }
+        public string NameOfFightingGame { get; set; }
+        public bool WinOrLose { get; set; }
+        public int NumberOfRounds { get; set; }
+
+        public int CharacterID { get; set; }
+        public virtual Character NameOfCharacter { get; set; }
+
+        public TimeSpan HowLongMinutes { get; set; }
+        public TimeSpan RecordTimeMinutes { get; set; }
+    }
+}

594720b46dc7fd4b45fd607af723601b00cef6db
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-04-11 14:29:44 -0500

    Ubuntu font, Blue links and navbar

--- VideoGameLogger/Models/Character.cs
+++ VideoGameLogger/Models/Character.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace VideoGameLogger.Models
     public class Character
     {
         public int CharacterID { get; set; }
+        [Display (Name = "Charater Name")]
         public string CharacterName { get; set; }
     }
 }
--- VideoGameLogger/Models/VideoGame.cs
+++ VideoGameLogger/Models/VideoGame.cs
@@ -12,13 +12,18 @@ namespace VideoGameLogger.Models
         [Required]
         [Display (Name = "Name of Fighting Game")]
         public string NameOfFightingGame { get; set; }
+        [Display(Name = "Won?")]
         public bool WinOrLose { get; set; }
+        [Display(Name = "Number of Rounds")]
         public int NumberOfRounds { get; set; }
 
+        [Display(Name = "Charater Name")]
         public int CharacterID { get; set; }
         public virtual Character NameOfCharacter { get; set; }
 
+        [Display(Name = "How Long in Minutes")]
         public TimeSpan HowLongMinutes { get; set; }
+        [Display(Name = "Record Time in Minutes")]
         public TimeSpan RecordTimeMinutes { get; set; }
     }
 }

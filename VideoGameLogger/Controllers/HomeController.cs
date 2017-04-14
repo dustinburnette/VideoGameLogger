4c599ea13a36d13668edd8567d642426dd02a8a1
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-04-14 14:45:41 -0500

    Added About and Contact page

--- VideoGameLogger/Controllers/HomeController.cs
+++ VideoGameLogger/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace VideoGameLogger.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Video Game Logger";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact me here: ";
 
             return View();
         }
